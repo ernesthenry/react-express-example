bcbc4bee740db88fce6f448ff5be85009b1a0fc6
ernesthenry <henry38ernest@gmail.com>
2020-06-08 16:57:30 +0300

    make it better

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', (req,res) => {
 
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req,res) => {
-    var list = ["item1", "item2", "item3"];
+    var list = ["item 1", "item 2", "item 3"];
     res.json(list);
     console.log('Sent list of items');
 });
